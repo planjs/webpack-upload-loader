207a16e24ec090723ec106a2c2d379df8fb948a6
fupeng <fupenglxy@gmail.com>
2020-08-10 11:49:33 +0800

    feat: add mimetype

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { getOptions, interpolateName } from 'loader-utils';
 import * as validateOptions from 'schema-utils';
 import * as ora from 'ora';
 import * as colors from 'colors';
+import * as mime from 'mime-types';
+import * as path from 'path';
 
 import * as schema from './options.json';
 
@@ -9,18 +11,48 @@ interface UploadParams {
   fileName: string;
   resourcePath: string;
   source: string;
+  mimetype: string;
 }
 
 interface webpackUploadLoaderOptions {
   esModule?: boolean;
   include?: string[];
   exclude?: string[];
+  mimetype?: string | boolean;
   upload?: (params: UploadParams) => Promise<string>;
   debug?: boolean;
 }
 
 function includeArr(arr: string[], p: string) {
   return arr.map(p => new RegExp(p, 'gi')).some(v => v.test(p));
+}
+
+function getMimetype(mimetype: string | boolean, resourcePath: string): string {
+  if (typeof mimetype === 'boolean') {
+    if (mimetype) {
+      const resolvedMimeType = mime.contentType(path.extname(resourcePath));
+
+      if (!resolvedMimeType) {
+        return '';
+      }
+
+      return resolvedMimeType.replace(/;\s+charset/i, ';charset');
+    }
+
+    return '';
+  }
+
+  if (typeof mimetype === 'string') {
+    return mimetype;
+  }
+
+  const resolvedMimeType = mime.contentType(path.extname(resourcePath));
+
+  if (!resolvedMimeType) {
+    return '';
+  }
+
+  return resolvedMimeType.replace(/;\s+charset/i, ';charset');
 }
 
 function webpackUploadLoader(source, map, meta) {
@@ -37,21 +69,20 @@ function webpackUploadLoader(source, map, meta) {
 
   const { resourcePath, rootContext } = this;
 
-  const fileName = interpolateName(this, '[contenthash].[ext][query]', {
-    context: resourcePath,
-    content: source,
-  });
-
-  const defaultReturn = () => {
+  const defaultCallback = () => {
     callback(null, source, map, meta);
   };
 
   if (exclude && includeArr(exclude, resourcePath)) {
-    return defaultReturn();
+    return defaultCallback();
   }
 
   if (include && includeArr(include, resourcePath)) {
-    console.log(resourcePath, fileName);
+    const mimetype = getMimetype(options.mimetype, resourcePath);
+    const fileName = interpolateName(this, '[contenthash].[ext][query]', {
+      context: resourcePath,
+      content: source,
+    });
     const spinner = ora(
       `uploading file: ${colors.green(`${fileName}`)} local: ${resourcePath.replace(
         rootContext,
@@ -63,6 +94,7 @@ function webpackUploadLoader(source, map, meta) {
         fileName,
         resourcePath,
         source,
+        mimetype,
       })
       .then(url => {
         spinner.succeed(`uploaded to: ${url}`);
@@ -78,7 +110,7 @@ function webpackUploadLoader(source, map, meta) {
         this.emitError(`${resourcePath} upload error: ${err?.toString()}`);
       });
   } else {
-    defaultReturn();
+    defaultCallback();
   }
 }
 
